af5e1089dd11ee4817de7ca4042dc40c69c3a7e1
Nguyen Uy Dinh <dinhnu@rikkeisoft.com>
2022-12-21 14:19:30 +0700

    add

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>Hello asdfahgsdfahsgdfashgd!</div>
+      <div>Hello asdfahgsdfahssadasdasdgdfashgd!</div>
     </div>
   );
 }
