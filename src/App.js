2112a983ba5f5325816bbe9c09b0c23d37756081
Nguyễn Uy Định <fantashy99@gmail.com>
2022-12-21 21:01:46 +0700

    acasc

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import './App.css';
 function App() {
   consolelog(123)
   console.log(asdasd)
-<<<<<<< Updated upstream
   console.log("them 1 cai nua")
-=======
   console.log("coment ms")
->>>>>>> Stashed changes
   return (
     <div className="App">
       
