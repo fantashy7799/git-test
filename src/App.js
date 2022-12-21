3e589873df5b1e3594032e6cfaa1da1a15a41803
Nguyễn Uy Định <fantashy99@gmail.com>
2022-12-21 20:58:35 +0700

    addv

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   consolelog(123)
   console.log(asdasd)
+<<<<<<< Updated upstream
   console.log("them 1 cai nua")
+=======
+  console.log("coment ms")
+>>>>>>> Stashed changes
   return (
     <div className="App">
       
